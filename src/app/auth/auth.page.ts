33491facfcab835f0498bfadaa804f6b73bb2861
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-03-28 18:56:49 +0800

    Set Discover page as the root page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./auth.service";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-auth",
@@ -14,6 +14,11 @@ export class AuthPage implements OnInit {
 
   onLogin() {
     this.authService.login(); // Authenticate
-    this.router.navigateByUrl('/places/discover'); // Go to main page
+    // this.router.navigateByUrl("/places/discover"); // Go to main page (Commented due to still can go back to Login page)
+    
+    // Problem: the use still can go back to the Login page after login. (Both iOS and Android)
+    // Solution: Set the following page into the root page after login authenticated.
+    // https://stackoverflow.com/questions/51427689/angular-5-remove-route-history
+    this.router.navigate(["/", "places", "discover"], { replaceUrl: true }); // Set the Discover page as the first page in the stack navigation.
   }
 }
