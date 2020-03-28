53b48a229542245a6e9a45aef5208b6703aef0a3
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-03-29 00:25:43 +0800

    Added Spinner in the Login page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -8,17 +8,24 @@ import { Router } from "@angular/router";
   styleUrls: ["./auth.page.scss"]
 })
 export class AuthPage implements OnInit {
+  isLoading: boolean = false;
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
   onLogin() {
+    this.isLoading = true;
     this.authService.login(); // Authenticate
     // this.router.navigateByUrl("/places/discover"); // Go to main page (Commented due to still can go back to Login page)
-    
+
     // Problem: the use still can go back to the Login page after login. (Both iOS and Android)
     // Solution: Set the following page into the root page after login authenticated.
     // https://stackoverflow.com/questions/51427689/angular-5-remove-route-history
-    this.router.navigate(["/", "places", "discover"], { replaceUrl: true }); // Set the Discover page as the first page in the stack navigation.
+    // We want to add a spinner here to show UX that the app is working.
+    // Put setTimeout to fake the app is communicating with the server.
+    setTimeout(() => {
+      this.isLoading = false;
+      this.router.navigate(["/", "places", "discover"], { replaceUrl: true }); // Set the Discover page as the first page in the stack navigation.
+    }, 1500);
   }
 }
