48e4b537b318ce3725a833ff5e420ec59ca5703f
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-03-30 00:38:23 +0800

    Finishing the New Offer Form template, Added moment.js

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Place } from "src/app/place.model";
 import { PlacesService } from "../../places.service";
+import * as moment from "moment";
 
 @Component({
   selector: "app-new-offer",
@@ -8,7 +9,18 @@ import { PlacesService } from "../../places.service";
   styleUrls: ["./new-offer.page.scss"]
 })
 export class NewOfferPage implements OnInit {
+  // Self add Moment.js (Based on old Ionic doc, it's also their official way to transform Date stuffs in the web.)
+  now: moment.Moment = moment();
+  availableFrom: string = this.now.format("YYYY-MM-DD").toString();
+  availableTo: string = this.now
+    .add(1, "year")
+    .format("YYYY-MM-DD")
+    .toString();
   constructor() {}
 
   ngOnInit() {}
+
+  onCreateOffer() {
+    console.log("Creating offered place...");
+  }
 }
